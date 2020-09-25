13b9af7abcf0e22ded1c966e5120e1bef50370a8
Matthew McCall <matthew@mccall.app>
2020-09-25 07:33:33 -0400

    Implemented support for ThomasDB and ThomasParser

--- src/main/java/app/mccall/thomas/ThomasCore.java
+++ src/main/java/app/mccall/thomas/ThomasCore.java
@@ -1,9 +1,27 @@
 package app.mccall.thomas;
 
+import java.util.Optional;
+
 public class ThomasCore {
 
     private ThomasDB db;
+    private ThomasParser parser;
 
-    public ThomasCore() {
+    public ThomasCore(ThomasDB db, ThomasParser parser) {
+        setDB(db);
+        setParser(parser);
     }
+
+    public void setDB(final ThomasDB db) {
+        this.db = db;
+    }
+
+    public void setParser(final ThomasParser parser) {
+        this.parser = parser;
+    }
+
+    public Optional<String> send(final String message) {
+        return Optional.empty();
+    }
+
 }
